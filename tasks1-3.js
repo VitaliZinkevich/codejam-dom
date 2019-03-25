765a24568406f3059d5a46cd5769f7ba2a92d11b
VitaliZinkevich <vitalizinkevich@gmail.com>
2019-03-25 15:57:44 +0300

    feat: add task3

--- tasks1-3.js
+++ tasks1-3.js
@@ -18,3 +18,32 @@ function WordReverse (str){
     return str.split('').reverse().join('')
 }
 
+
+function ChangeLetters (str) {
+    let alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+    let toUpper = 'aeiou'.split('');
+    
+    let newStrArr=[]
+
+    let strArr = str.split('')
+
+    strArr.forEach((e)=>{
+
+        if (alphabet.indexOf(e) !== -1) {
+            let currIndex= alphabet.indexOf(e)
+            e = alphabet[currIndex+1] || 'a'
+
+            if (toUpper.indexOf(e) !== -1) {
+                e = e.toUpperCase()
+            }
+            newStrArr.push(e)
+        } else {
+            newStrArr.push(e)
+        }
+
+
+    })
+
+    return newStrArr.join('')
+
+}
